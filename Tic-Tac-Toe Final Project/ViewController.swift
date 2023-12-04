573dc5ef72de2ba1b7a2b9472a2167d90571afb5
Tyler Mok <tymok06@gmail.com>
2023-12-04 10:01:15 -0600

    Initial Commit

---
+++ Tic-Tac-Toe Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe Final Project
+//
+//  Created by TYLER MOK on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
